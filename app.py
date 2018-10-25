59299d148982eb375c73e49fd7b5cd0af3b92b5b
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-10-25 15:46:36 +0100

    update app.

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask
+import boto3
 app = Flask(__name__)
+rds = boto3.client('rds')
+
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!hehe change 2 heh jusia kocia lololol. aaa bb c d koszka zzz a'
+    try:
+        dbs = rds.describe_db_instances()
+        for db in dbs['DSInstances']:
+            return((%s@%s:s %s) % (db['MasterUsername'], db['Endpoint']['Address'], db['Endpoint']['Port'], db['DBInstanceStatus']))
+    except Exceptions as e:
+        return e
+
+    # return 'Hello, World!hehe change 2 heh jusia kocia lololol. aaa bb c d koszka zzz a'
