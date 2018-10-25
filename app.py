41a8ef178f47c91ed854de2988748ab36e72a77e
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-10-25 16:07:38 +0100

    try show dbnumber.

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
-# import boto3
+import boto3
 app = Flask(__name__)
-# rds = boto3.client('rds', region_name='us-west-1')
+rds = boto3.client('rds', region_name='us-west-1')
 
 
 @app.route('/')
 def hello_world():
-    # try:
-    #     dbs = rds.describe_db_instances()
-    #     length = len(dbs)
-    #     return length
+    try:
+        dbs = rds.describe_db_instances()
+        length = len(dbs)
+        return length.str()
+    except Exception as e:
+        return e
     #     for db in dbs['DSInstances']:
     #         return((%s@%s:s %s) % (db['MasterUsername'], db['Endpoint']['Address'], db['Endpoint']['Port'], db['DBInstanceStatus']))
     # except Exceptions as e:
     #     return e
 
-    return 'Hello, World!hehe change 2 heh jusia kocia lololol. aaa bb c d koszka zzz aIgore kotore'
+    # return 'Hello, World!hehe change 2 heh jusia kocia lololol. aaa bb c d koszka zzz aIgore kotore'
