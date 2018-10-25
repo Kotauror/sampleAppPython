5fc0c35aed716dc584d2d55ef029abe25f511f99
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-10-25 15:48:29 +0100

    igr.

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ rds = boto3.client('rds')
 
 @app.route('/')
 def hello_world():
-    try:
-        dbs = rds.describe_db_instances()
-        for db in dbs['DSInstances']:
-            return((%s@%s:s %s) % (db['MasterUsername'], db['Endpoint']['Address'], db['Endpoint']['Port'], db['DBInstanceStatus']))
-    except Exceptions as e:
-        return e
+    # try:
+    #     dbs = rds.describe_db_instances()
+    #     for db in dbs['DSInstances']:
+    #         return((%s@%s:s %s) % (db['MasterUsername'], db['Endpoint']['Address'], db['Endpoint']['Port'], db['DBInstanceStatus']))
+    # except Exceptions as e:
+    #     return e
 
-    # return 'Hello, World!hehe change 2 heh jusia kocia lololol. aaa bb c d koszka zzz a'
+    return 'Hello, World!hehe change 2 heh jusia kocia lololol. aaa bb c d koszka zzz aIgore'
