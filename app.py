1396e4aee1d27b0d93a3e65ada03d20e41cf7944
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-10-29 10:19:14 +0000

    use sqlachment.

--- app.py
+++ app.py
@@ -1,17 +1,30 @@
 from flask import Flask
 import boto3
-app = Flask(__name__)
-rds = boto3.client('rds', region_name='us-west-1')
+import os
+from flask_sqlalchemy import SQLAlchemy
 
-def getNumberOfDBs():
-    try:
-        dbs = rds.describe_db_instances()
-        return len(dbs)
-    except Exception as e:
-        return e
+# app = Flask(__name__)
+# rds = boto3.client('rds', region_name='us-west-1')
+
+// config
+# SQLALCHEMY_DATABASE_URI = os.environ.get(
+#         'DATABASE_URI', 'postgresql://localhost/contacts')
+
+app = Flask(__name__)
+# app.config.from_object(SQLALCHEMY_DATABASE_URI)
+db = SQLAlchemy(app)
+
+
+# def getNumberOfDBs():
+#     try:
+#         dbs = rds.describe_db_instances()
+#         return len(dbs)
+#     except Exception as e:
+#         return e
 
 @app.route('/')
 def hello_world():
-    numOfDB = getNumberOfDBs()
-    print(numOfDB)
-    return "Hi Mentors! My EC2 instance has: " + str(numOfDB) + " databases, I think it should have one....."
+    # numOfDB = getNumberOfDBs()
+    # print(numOfDB)
+    return "Im using sqlalchemy"
+    # return "Hi Mentors! My EC2 instance has: " + str(numOfDB) + " databases, I think it should have one....."
