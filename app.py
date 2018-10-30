762d6f0e26318be3ad424f608778fafcecff8035
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-10-30 13:30:38 +0000

    add templates.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from contact import db
 from app_config import app
 from flask_sqlalchemy import SQLAlchemy
@@ -9,6 +9,6 @@ db.init_app(app)
 
 @app.route('/')
 def hello_world():
-    numOfCont = contacts_book.get_number_of_contacts()
-    contactsString = contacts_book.get_contacts_names_as_string()
-    return "There are " + str(numOfCont) + " in the db and the contact names are: " + contactsString
+    contacts = contacts_book.get_contacts()
+    number_of_contacts = contacts_book.get_number_of_contacts()
+    return render_template('index.html', number_of_contacts = number_of_contacts, contacts = contacts)
--- contacts_book.py
+++ contacts_book.py
@@ -7,10 +7,3 @@ class ContactsBook():
 
     def get_number_of_contacts(self):
         return len(Contact.query.all())
-
-    def get_contacts_names_as_string(self):
-        contacts = Contact.query.all()
-        str_list = []
-        for contact in contacts:
-            str_list.append(contact.name)
-        return ', '.join(str_list)
